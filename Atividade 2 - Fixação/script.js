1efee116607bfc88caaaace82b555dec931e6cb4
Rafael Felisbino Rocha da Silva <72631228+RafaelFelisbino-hub@users.noreply.github.com>
2021-04-01 11:41:22 -0300

    Add files via upload

---
+++ Atividade 2 - Fixação/script.js
@@ -0,0 +1,39 @@
+// INVERNO
+
+function invernoOn(){
+    document.querySelector("#inverno").innerHTML = "De 21 de junho a <br>23 de setembro";
+}
+
+function invernoOff(){
+    document.querySelector("#inverno").innerHTML = "";
+}
+
+// PRIMAVERA
+
+function primaveraOn(){
+    document.querySelector("#primavera").innerHTML = "De 23 de setembro a <br>21 de dezembro.";
+}
+
+function primaveraOff(){
+    document.querySelector("#primavera").innerHTML = "";
+}
+
+// VERAO
+
+function veraoOn(){
+    document.querySelector("#verao").innerHTML = "De 21 de dezembro a <br>21 de março.";
+}
+
+function veraoOff(){
+    document.querySelector("#verao").innerHTML = "";
+}
+
+// OUTONO
+
+function outonoOn(){
+    document.querySelector("#outono").innerHTML = "De 21 de março a <br>21 de junho";
+}
+
+function outonoOff(){
+    document.querySelector("#outono").innerHTML = "";
+}
